c24155e92c20eab6e9c15b5c348a794d78e35080
Ivan <xpynkostb@gmail.com>
2022-04-13 20:00:27 +0300

    fixing bg

--- client/components/game.js
+++ client/components/game.js
@@ -64,9 +64,9 @@ const Game = () => {
   }, [newArray])
 
   return (
-    <div className="min-h-screen h-full min-w-min flex justify-center">
+    <div className="min-h-screen min-w-min flex justify-center bg-gray-300">
       <Head title="Game" />
-      <div className="flex flex-col flex-wrap items-center justify-center">
+      <div className="h-screen flex flex-col flex-wrap items-center justify-center">
         <div className="m-auto p-10 bg-gray-900 rounded-lg">
           <div
             style={{
